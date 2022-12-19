33aafd6478c8eadd54ab21c83d956b1f2364e79b
Hyeyoung-Lucia <hyeyoung9115@gmail.com>
2022-12-19 14:53:09 +0900

    ⚡️Feat: Add hamburger menu toggling interaction #5

--- app/js/sidebar.js
+++ app/js/sidebar.js
@@ -2,17 +2,27 @@ const sidebarMenuButton = document.querySelector('.gnb-icon-button')
 const sidebarMenu = document.querySelector('.sidebar')
 const sidebarOverlay = document.querySelector('.overlay')
 const sidebarList = document.querySelector('.sidebar-nav-list')
+const sidebarHamburgerMenu = document.querySelector('#hamburger-btn')
 
-function ToggleSidebarMenuBtn() {
+function toggleSidebarMenuBtn() {
   sidebarMenu.classList.toggle('is-active')
   sidebarOverlay.classList.toggle('is-active')
+  sidebarMenuButton.classList.toggle('toggle')
+  sidebarHamburgerMenu.classList.toggle('toggle')
 }
 
-sidebarMenuButton.addEventListener('click', ToggleSidebarMenuBtn)
+function handleChangeBtn() {
+  toggleSidebarMenuBtn()
+}
+
+sidebarMenuButton.addEventListener('click', toggleSidebarMenuBtn)
+sidebarHamburgerMenu.addEventListener('click', handleChangeBtn)
 
 function closeSidebarMenu() {
   sidebarMenu.classList.remove('is-active')
   sidebarOverlay.classList.remove('is-active')
+  sidebarMenuButton.classList.toggle('toggle')
+  sidebarHamburgerMenu.classList.toggle('toggle')
 }
 
 sidebarOverlay.addEventListener('click', closeSidebarMenu)
